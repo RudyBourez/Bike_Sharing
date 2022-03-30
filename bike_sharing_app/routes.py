7932ecfabda0e708f1411bc2505eecd263137888
Rudy <bourez.rudy@gmail.com>
2022-03-30 09:22:24 +0200

    last modofications

--- bike_sharing_app/routes.py
+++ bike_sharing_app/routes.py
@@ -53,22 +53,6 @@ def prediction():
 def statistics():
     return render_template('Statistics.html')
 
-@app.route("/predict", methods=["GET", "POST"])
-def predict():
-    form = Prediction_form()
-    if form.validate_on_submit():
-        year = form.date.data.year
-        month = form.date.data.month
-        day = form.date.data.day
-        hour = form.hour.data
-        df_pred = pd.read_csv("Datas/data_test.csv")
-        data = df_pred[(df_pred["month"]== month) & (df_pred["day_number"] == day) & (df_pred["hour"]== hour) & (df_pred["year"] == year)]
-        model = pickle.load(open("Modèles/modele.sav", "rb"))
-        pred = np.exp(model.predict(data)) - 1
-        return redirect(url_for("afficher_pred", pred=pred))
-    return render_template("predict.html", form=form)
-
-
 @app.route("/afficher_pred")
 def afficher_pred():
     pred = request.args.get("pred")
