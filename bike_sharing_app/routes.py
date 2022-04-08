37e020a20303081e774975aeb5b1d795336b9db3
Rudy <bourez.rudy@gmail.com>
2022-04-08 08:57:41 +0200

    .

--- bike_sharing_app/routes.py
+++ bike_sharing_app/routes.py
@@ -36,10 +36,22 @@ def logout():
     flash("Logged out with success", category="success")
     return redirect(url_for("home"))
 
+@app.route("/make_pred", methods=["GET","POST"])
+@login_required
+def make_pred():
+    form = PredictionForm()
+    if form.validate_on_submit():
+        hour = form.hour.data
+        date = form.date.data.strftime('%d-%m-%Y')
+        month = form.date.data.month
+        day = form.date.data.day
+        return redirect(url_for("prediction",month=month, day=day, hour=hour, date=date))
+    return render_template("afficher.html", form=form)
+
 @app.route("/table_prediction")
 @login_required
 def table_prediction():
-    df = create_df(with_meteo=True)
+    df = create_df()
     #modele : 
     liste = []
     liste_weather = []
@@ -67,22 +79,10 @@ def table_prediction():
 
     return render_template("table_prediction.html",pred = df_pred.to_dict(orient="split"),graphJSON=graphJSON,graphJSON_moyen=graphJSON_moyen)
 
-@app.route("/make_pred", methods=["GET","POST"])
-@login_required
-def make_pred():
-    form = PredictionForm()
-    if form.validate_on_submit():
-        hour = form.hour.data
-        date = form.date.data.strftime('%d-%m-%Y')
-        month = form.date.data.month
-        day = form.date.data.day
-        return redirect(url_for("prediction",month=month, day=day, hour=hour, date=date))
-    return render_template("afficher.html", form=form)
-
 @app.route("/prediction/<date>&<month>&<day>&<hour>")
 @login_required
 def prediction(date, month, day, hour):
-    df = create_df(with_meteo=False, month=int(month), day=int(day), hour=int(hour))
+    df = forecast(month=int(month), day=int(day), hour=int(hour))
     response = requests.get(f"https://bike-sharing-rfm-api.herokuapp.com/{df.to_json(orient='columns')}")
     count = int(list(eval(response.json())["count"].values())[0])
     registered = int(list(eval(response.json())["registered"].values())[0])
@@ -91,55 +91,56 @@ def prediction(date, month, day, hour):
     return render_template("Forecast.html", date=date, hour=hour, count=count,
     registered=registered, casual=casual, weather=weather)
 
-def create_df(with_meteo:bool, day=None, month=None, hour=None):
-    """Allow to create a dataframe for a prediction depending on knowledge of meteorologicals datas"""
-    if with_meteo:
-        df = pd.DataFrame()
-        client = MeteoFranceClient()
-        weather_forecast = client.get_forecast(latitude=50.62925, longitude=3.057256)
-        forecast = weather_forecast.forecast
-        df["temp"] = [d["T"]["value"] for d in forecast]
-        df["humidity"] = [d["humidity"] for d in forecast]
-        df["windspeed"] = [d["wind"]["speed"] for d in forecast]
-        df["datetime"] = [datetime.fromtimestamp(d["dt"]).strftime("%m %d") for d in forecast]
-        df["hour"] = [datetime.fromtimestamp(d["dt"]).hour for d in forecast]
-        df["year"] = [datetime.fromtimestamp(d["dt"]).year for d in forecast]
-        df["month"] = [datetime.fromtimestamp(d["dt"]).month for d in forecast]
-        df["day_number"] = [datetime.fromtimestamp(d["dt"]).day for d in forecast]
-        df["day"] = [datetime.fromtimestamp(d["dt"]) for d in forecast]
-        df["day"] = df["day"].dt.strftime("%A %d. %B %Y").str.extract(r'(\w+)\s')
-        df["workingday"] = [int(d not in ["Saturday","Sunday"]) for d in df["day"]]
-        #liste des jours de vacances : (month,day)
-        liste_holiday = [(7, 4),(4, 16),(1, 2),(9, 3),(10, 8),(1, 17),(4, 15),(9, 5),(10, 10),(11, 12),(1, 16),(11, 11)]
-        season = []
-        holiday = []
-        for i in range(len(df)):
-            month = df.loc[i]["month"]
-            day = df.loc[i]["day_number"]
+def create_df():
+    """Allow to create a dataframe for a prediction with meteofrance-api"""
+    client = MeteoFranceClient()
+    forecast = client.get_forecast(latitude=50.62925, longitude=3.057256).forecast
+    df = pd.DataFrame.from_dict(forecast).iloc[:, :5].drop("sea_level",axis=1)
+    df["datetime"] = [datetime.fromtimestamp(line).strftime("%m %d") for line in df["dt"]]
+    df["hour"] = [datetime.fromtimestamp(line).hour for line in df["dt"]]
+    df["year"] = [datetime.fromtimestamp(line).year for line in df["dt"]]
+    df["month"] = [datetime.fromtimestamp(line).month for line in df["dt"]]
+    df["windspeed"] = [item["speed"] for item in df["wind"]]
+    df["temp"] = [item["value"] for item in df["T"]]
+    df["day_number"] = [datetime.fromtimestamp(line).day for line in df["dt"]]
+    df["day"] = [datetime.fromtimestamp(line).strftime("%A") for line in df["dt"]]
+    df["workingday"] = [int(d not in ["Saturday","Sunday"]) for d in df["day"]]
+    
+    #liste des jours de vacances : (month,day)
+    liste_holiday = [(7, 4),(4, 16),(1, 2),(9, 3),(10, 8),(1, 17),(4, 15),(9, 5),(10, 10),(11, 12),(1, 16),(11, 11)]
+    season = []
+    holiday = []
+    for i in range(len(df)):
+        month = df.loc[i]["month"]
+        day = df.loc[i]["day_number"]
 
-            # vacances ?
-            if (month,day) in liste_holiday:
-                holiday.append(1)
-            else:
-                holiday.append(0)
+        # vacances ?
+        if (month,day) in liste_holiday:
+            holiday.append(1)
+        else:
+            holiday.append(0)
 
-            # saison :
-            if  "03 20" <= df.loc[i]["datetime"] <= "06 20":
-                season.append(1)
-            elif  "06 21" <= df.loc[i]["datetime"] <= "09 22":
-                season.append(2)
-            elif  "09 23" < df.loc[i]["datetime"] < "12 22":
-                season.append(3)
-            else:
-                season.append(4)
+        # saison :
+        if  "03 20" <= df.loc[i]["datetime"] <= "06 20":
+            season.append(1)
+        elif  "06 21" <= df.loc[i]["datetime"] <= "09 22":
+            season.append(2)
+        elif  "09 23" < df.loc[i]["datetime"] < "12 22":
+            season.append(3)
+        else:
+            season.append(4)
 
-        df["season"] = season
-        df["holiday"] = holiday
-
-    else:
-        df = pd.read_csv("bike_sharing_app/all_data.csv")
-        df.drop("atemp",axis=1)
-        df = df[(df["day_number"]==day) & (df["hour"] == hour) & (df["month"] == month) & (df["year"] == 2011)]
-        df.drop(["weather", "count", "atemp"], axis=1, inplace=True)
-
+    df["season"] = season
+    df["holiday"] = holiday
+    
     return df.drop("datetime", axis=1)
+
+def forecast(day, month, hour):
+    """Allow to create a dataframe for a prediction by collecting meteorological informations
+    in all_data at a specific date in 2011"""
+    
+    df = pd.read_csv("bike_sharing_app/all_data.csv")
+    df.drop("atemp",axis=1)
+    df = df[(df["day_number"]==day) & (df["hour"] == hour) & (df["month"] == month) & (df["year"] == 2011)]
+    df.drop(["weather", "count", "atemp", "datetime"], axis=1, inplace=True)
+    return df
